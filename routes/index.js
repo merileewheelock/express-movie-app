ff60c3a820830aa031c8cd87e077c53dafcba082
merilee-wheelock <eelirem@gmail.com>
2017-06-06 15:38:56 -0400

    add password hashing and login page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var config = require('../config/config');
+var bcrypt = require('bcrypt-nodejs');
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -25,10 +26,12 @@ router.get('/', function(req, res, next) {
 	request.get(nowPlayingUrl,(error, response, movieData)=>{
 		var movieData = JSON.parse(movieData);
 		// console.log(movieData);
+		console.log(req.session);
 		res.render('movie_list', { 
 			movieData: movieData.results,
 			imageBaseUrl: imageBaseUrl,
-			titleHeader: "Movie Database"
+			titleHeader: "Movie Database",
+			sessionInfo: req.session
 		});
 	});
 });
@@ -77,6 +80,74 @@ router.get('/movie/:id', (req, res)=>{
 		});
 	});
 	// res.send(req.params.id);
+});
+
+router.get('/register', (req, res)=>{
+	// res.send("This is the register page");
+	var message = req.query.msg;
+	if (message == "badEmail"){
+		message = "This email is already registered";
+	}
+	res.render('register', { message: message });
+});
+
+router.post('/registerProcess', (req, res)=>{
+	// res.json(req.body);
+	var name = req.body.name;
+	var email = req.body.email;
+	var password = req.body.password;
+	var hash = bcrypt.hashSync(password);
+	console.log(hash);
+
+	var selectQuery = "SELECT * FROM users WHERE email = ?";
+	connection.query(selectQuery,[email],(error, results)=>{
+		if (results.length == 0){
+			// User is not in the db. Insert them.
+			var insertQuery = "INSERT INTO users (name,email,password) VALUES (?,?,?)";
+			connection.query(insertQuery, [name,email,hash], (error, results)=>{
+				// Add session vars -- name, email, loggedin, id
+				req.session.name = name;
+				req.session.email = email;
+				req.session.loggedin = true;
+				res.redirect('/?msg=registered');
+			});
+		}else{
+			// User is in the db. Send them back to register with a message.
+			res.redirect('/register?msg=badEmail');
+		}
+	});
+});
+
+router.get('/login', (req, res)=>{
+	// res.send("This is the login page");
+	res.render('login', {});
+});
+
+router.post('/processLogin', (req, res)=>{
+	// res.json(req.body);
+	var email = req.body.email;
+	var password = req.body.password;
+	// var selectQuery = "SELECT * FROM users WHERE email = ? AND password = ?";
+	var selectQuery = "SELECT * FROM users WHERE email = ?";
+	connection.query(selectQuery, [email], (error,results)=>{
+		if (results.length == 1){
+			// Match found!
+			// Check to see if the password matches
+			var match = bcrypt.compareSync(password, results[0].password); // true
+			if (match == true){
+					// We passed the english pass and hash thrugh compareSync, and they match
+				req.session.loggedin = true;
+				req.session.name = results[0].name;
+				req.session.email = results[0].email;
+				res.redirect('/?msg=loggedin')
+			}else{
+				res.redirect('/login?msg=badLogin');
+			}
+		}else{
+			// This isn'tthe droid we are looking for.
+			res.redirect('/login?msg=badLogin');
+		}
+	});
 });
 
 module.exports = router;
